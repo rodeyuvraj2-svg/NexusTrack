686ffdd7236d1dc74cc09c4dcb3920c86dfd9641
Yuvraj Rode <rodeyuvraj2@gmail.com>
2026-09-24 22:31:29 +0530

    fix route errors

--- src/routes/discover-redirect.tsx
+++ src/routes/discover-redirect.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router';
 import { redirect } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/discover-redirect')({
   beforeLoad: () => {
-    throw redirect({ to: '/_authenticated/discover' });
+    throw redirect({ to: '/discover' });
   },
 });
